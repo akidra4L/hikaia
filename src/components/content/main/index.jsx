3bdbd781192b307a4e6e3ec210b087067da5bc1a
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-21 16:57:41 +0600

    hikaia

--- src/components/content/main/index.jsx
+++ src/components/content/main/index.jsx
@@ -1,13 +1,47 @@
 import React from "react";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineOppositeContent,
+} from "@mui/lab";
+
+// Data
 import kazakhstanData from "../../../data/kazakhstan.json";
 import worldData from "../../../data/world.json";
 
 export const Main = () => {
   return (
     <div className="main">
-      {kazakhstanData.map((item) => (
-        <div className="main-wrapper">
-          {item.events}
+      {kazakhstanData.map((kzItem) => (
+        <div>
+          {worldData.map((worldItem) => (
+            <div>
+              <Timeline position="alternate">
+                <TimelineItem>
+                  <div className="column">
+                    {kzItem.events.map((event) => (
+                      <TimelineOppositeContent>{event}</TimelineOppositeContent>
+                    ))}
+                  </div>
+                  <TimelineSeparator>
+                    <TimelineDot>
+                      year
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <div className="column">
+                    {worldItem.events.map((event) => (
+                      <TimelineContent>{event}</TimelineContent>
+                    ))}
+                  </div>
+                </TimelineItem>
+              </Timeline>
+            </div>
+          ))}
         </div>
       ))}
     </div>
