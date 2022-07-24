213fc013571e0df6fa7a915e6e1b796f4b0c820c
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-24 12:26:52 +0600

    hikaia

--- src/components/content/main-block/index.jsx
+++ src/components/content/main-block/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 export const MainBlock = () => {
-    return (
-        <div className="mainblock">
-            <div className="mainblock-wrapper">
-                <p>Привет, Казахстан!</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="mainblock">
+      <div className="mainblock-wrapper">
+        <p>Привет, Казахстан!</p>
+        <p>Данный сайт был сделан с целью показать и сравнить,</p>
+        <p>
+          Как менялась наша страна со времен образования Казахского Ханства,
+        </p>
+        <p>И что же происходило в Мире в это же время</p>
+      </div>
+    </div>
+  );
+};
