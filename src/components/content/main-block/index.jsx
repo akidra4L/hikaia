8ab6850e741a149189c111c5d122b12911af772e
Alikhan Gubayev <AlikhanGubayev@MacBook-Air-Air.local>
2022-07-25 16:20:09 +0600

    hikaia

--- src/components/content/main-block/index.jsx
+++ src/components/content/main-block/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 export const MainBlock = () => {
   return (
     <div className="mainblock">
       <div className="mainblock-wrapper">
         <h2>Привет, Казахстан!</h2>
-        <p>
-          &nbsp;Данный сайт был сделан с целью показать и сравнить, как
-          менялась наша страна со времен образования Казахского Ханства, и что
-          же происходило в Мире в это же время
-        </p>
+        <Typewriter
+          options={{
+            strings: [
+              "Данный сайт был сделан с целью показать и сравнить, как менялась наша страна со времен образования Казахского Ханства, и что же происходило в Мире в это же время",
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
       </div>
     </div>
   );
