eb8207ca5465345e6772bfa37b30e94234e41ac0
Alikhan Gubayev <jonasofan.gubaev@gmail.com>
2022-08-01 21:58:45 +0600

    hikaia

--- src/components/content/source/index.jsx
+++ src/components/content/source/index.jsx
@@ -1,5 +1,3 @@
-import warrior from "../../../assets/warrior.gif";
-
 export const Source = () => {
   return (
     <div className="source">
@@ -13,18 +11,32 @@ export const Source = () => {
               <a
                 href="https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"
                 target="_blank"
+                rel="noreferrer"
               >
                 Wikipedia
               </a>
             </li>
             <li>
-              <a href="https://e-history.kz/ru/news/show/4288/" target="_blank">
+              <a
+                href="https://e-history.kz/ru/news/show/4288/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Qazaqstan Tarihy
               </a>
             </li>
             <li>
-              <a href="https://adebiportal.kz/ru" target="_blank">
+              <a
+                href="https://adebiportal.kz/ru"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Adebiportal
+              </a>
+            </li>
+            <li>
+              <a href="https://nomad.su/" target="_blank" rel="noreferrer">
+                Nomad
               </a>
             </li>
           </ul>
